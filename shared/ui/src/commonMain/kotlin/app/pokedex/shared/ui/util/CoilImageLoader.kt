026b3c2e54a912739fa275c1be9df4a14861f29a
Usmon Abdurakhmanov <usmonwastaken@gmail.com>
2024-07-11 17:58:21 +0500

    refactor: Setup Coil Image Loader
    This commit sets up the Coil Image Loader for use in the application.
    
    - Introduces a composable function to configure the ImageLoader
     with memory caching, crossfading, and debugging.

--- shared/ui/src/commonMain/kotlin/app/pokedex/shared/ui/util/CoilImageLoader.kt
+++ shared/ui/src/commonMain/kotlin/app/pokedex/shared/ui/util/CoilImageLoader.kt
@@ -1,9 +1,19 @@
 package app.pokedex.shared.ui.util
 
+import androidx.compose.runtime.Composable
 import coil3.ImageLoader
-import coil3.PlatformContext
+import coil3.compose.setSingletonImageLoaderFactory
 import coil3.request.CachePolicy
 import coil3.request.crossfade
 import coil3.util.DebugLogger
 
-
+@Composable
+internal fun SetupCoilImageLoader() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()
+    }
+}
